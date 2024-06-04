0fbda75e78acba66ec7156870b6875f65172ce39
pseudobabble <harryjohnson500@gmail.com>
2024-06-04 14:07:24 +0100

    update unit arithmetic checks

--- src/types.rs
+++ src/types.rs
@@ -10,36 +10,108 @@ pub enum BinaryOperation {
     Divide,
 }
 
-/// This is the conversion factor to base units
-#[derive(PartialEq, PartialOrd, Debug, Clone)]
-pub enum UnitConversionRate {
-    None = 1,
-    Second = 0.000011574074074074073499,
-    Minute = 0.0006944444444444445,
-    Hour = 0.041666666666666664,
-    Day = 1,
-    Meter = 1,
-    Kilometer = 1000,
-    USD = 1,
-    GBP = 0.8,
+#[derive(PartialEq, Debug, Clone, Copy)]
+pub enum UnitIdentity {
+    None,
+    Second,
+    Minute,
+    Hour,
+    Day,
+    Meter,
+    Kilometer,
+    USD,
+    GBP,
 }
 
-#[derive(PartialEq, PartialOrd, Debug, Clone)]
+#[derive(Debug, Clone)]
 pub struct Unit {
-    conversion_rate: UnitConversionRate,
+    unit: HashMap<UnitIdentity, i64>,
     exponent: i64,
     dimension: Dimension,
 }
 
-#[derive(PartialEq, PartialOrd, Debug, Clone)]
-pub enum DimensionIdentity {
-    None(UnitConversionRate::None),
-    Time(UnitConversionRate::Hour),
-    Length(UnitConversionRate::Meter),
-    Currency(UnitConversionRate::USD),
+// TODO: this wont work, discriminant is for enum
+impl Add for Unit {
+    type Output = Unit;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        // Check all keys match between self and rhs, and they have the same length
+        if self.dimension.keys().all(|k| rhs.dimension.contains_key(k))
+            && self.dimension.len() == rhs.dimension.len()
+        {
+            self
+        } else {
+            panic!("Can't subtract rhs: {:#?} from lhs: {:#?}", rhs, self)
+        }
+    }
 }
 
-#[derive(PartialEq, PartialOrd, Debug, Clone)]
+impl Sub for Unit {
+    type Output = Unit;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        // Check all keys match between self and rhs, and they have the same length
+        if self.dimension.keys().all(|k| rhs.dimension.contains_key(k))
+            && self.dimension.len() == rhs.dimension.len()
+        {
+            self
+        } else {
+            panic!("Can't subtract rhs: {:#?} from lhs: {:#?}", rhs, self)
+        }
+    }
+}
+
+impl Mul for Unit {
+    type Output = Unit;
+
+    fn mul(self, rhs: Self) -> Self {
+        let mut result = self.unit.clone();
+        for (key, value) in rhs.unit {
+            *result.entry(key.clone()).or_insert(0) += value;
+        }
+
+        Unit { unit: result }
+    }
+}
+
+impl Div for Unit {
+    type Output = Unit;
+
+    fn div(self, rhs: Self) -> Self {
+        let mut result = self.unit.clone();
+        for (key, value) in rhs.unit {
+            *result.entry(key.clone()).or_insert(0) -= value;
+        }
+
+        Unit { unit: result }
+    }
+}
+
+impl Unit {
+    pub fn conversion_rate(self) -> f64 {
+        match self.identity {
+            UnitIdentity::None => 1.0,
+            UnitIdentity::Second => 0.000011574074074074073499,
+            UnitIdentity::Minute => 0.0006944444444444445,
+            UnitIdentity::Hour => 0.041666666666666664,
+            UnitIdentity::Day => 1.0,
+            UnitIdentity::Meter => 1.0,
+            UnitIdentity::Kilometer => 1000.0,
+            UnitIdentity::USD => 1.0,
+            UnitIdentity::GBP => 0.8,
+        }
+    }
+}
+
+#[derive(PartialEq, Eq, Hash, Debug, Clone)]
+pub enum DimensionIdentity {
+    None,
+    Time,
+    Length,
+    Currency,
+}
+
+#[derive(Debug, Clone)]
 pub struct Dimension {
     dimension: HashMap<DimensionIdentity, i64>,
 }
@@ -47,11 +119,14 @@ pub struct Dimension {
 impl Add for Dimension {
     type Output = Dimension;
 
-    fn add(self, rhs: Self) -> Self {
-        if discriminant(self) == discriminant(rhs) {
+    fn add(self, rhs: Self) -> Self::Output {
+        // Check all keys match between self and rhs, and they have the same length
+        if self.dimension.keys().all(|k| rhs.dimension.contains_key(k))
+            && self.dimension.len() == rhs.dimension.len()
+        {
             self
         } else {
-            panic!("Can't add lhs: {:#?} to rhs: {:#?}", self, rhs)
+            panic!("Can't subtract rhs: {:#?} from lhs: {:#?}", rhs, self)
         }
     }
 }
@@ -60,7 +135,10 @@ impl Sub for Dimension {
     type Output = Dimension;
 
     fn sub(self, rhs: Self) -> Self {
-        if discriminant(self) == discriminant(rhs) {
+        // Check all keys match between self and rhs, and they have the same length
+        if self.dimension.keys().all(|k| rhs.dimension.contains_key(k))
+            && self.dimension.len() == rhs.dimension.len()
+        {
             self
         } else {
             panic!("Can't subtract rhs: {:#?} from lhs: {:#?}", rhs, self)
@@ -268,8 +346,8 @@ impl Add for DimensionedValue {
     fn add(self, rhs: Self) -> Self {
         println!("\n\nAdding {:#?} to {:#?}", self, rhs);
 
-        let lhs_in_base_units = self.value * Value::Float(self.unit.conversion_rate);
-        let rhs_in_base_units = rhs.value * Value::Float(rhs.unit.conversion_rate);
+        let lhs_in_base_units = self.value * Value::Float(self.unit.conversion_rate());
+        let rhs_in_base_units = rhs.value * Value::Float(rhs.unit.conversion_rate());
 
         let value = lhs_in_base_units + rhs_in_base_units;
 
@@ -279,6 +357,8 @@ impl Add for DimensionedValue {
             value: value,
             unit: Unit {
                 dimension: self.unit.dimension + rhs.unit.dimension,
+                exponent: self.unit.exponent,
+                unit: self.unit.unit + rhs.unit.unit,
             },
         }
     }
@@ -290,17 +370,20 @@ impl Sub for DimensionedValue {
     fn sub(self, rhs: Self) -> Self {
         println!("\n\nSubtracting {:#?} from {:#?}", rhs, self);
 
-        let lhs_in_base_units = self.value * Value::Float(self.unit.conversion_rate);
-        let rhs_in_base_units = rhs.value * Value::Float(rhs.unit.conversion_rate);
+        let lhs_in_base_units = self.value * Value::Float(self.unit.conversion_rate());
+        let rhs_in_base_units = rhs.value * Value::Float(rhs.unit.conversion_rate());
 
         let value = lhs_in_base_units - rhs_in_base_units;
 
         println!("\nResult = {:#?}", value);
 
+        // TODO: validate exponent?
         DimensionedValue {
             value: value,
             unit: Unit {
                 dimension: self.unit.dimension - rhs.unit.dimension,
+                exponent: self.unit.exponent,
+                unit: self.unit.unit - rhs.unit.unit,
             },
         }
     }
@@ -312,8 +395,8 @@ impl Mul for DimensionedValue {
     fn mul(self, rhs: Self) -> Self {
         println!("\n\nMultiplying {:#?} with {:#?}", self, rhs);
 
-        let lhs_in_base_units = self.value * Value::Float(self.unit.conversion_rate);
-        let rhs_in_base_units = rhs.value * Value::Float(rhs.unit.conversion_rate);
+        let lhs_in_base_units = self.value * Value::Float(self.unit.conversion_rate());
+        let rhs_in_base_units = rhs.value * Value::Float(rhs.unit.conversion_rate());
 
         let value = lhs_in_base_units * rhs_in_base_units;
 
@@ -323,6 +406,8 @@ impl Mul for DimensionedValue {
             value: value,
             unit: Unit {
                 dimension: self.unit.dimension * rhs.unit.dimension,
+                exponent: self.unit.exponent,
+                unit: self.unit.unit * rhs.unit.unit,
             },
         }
     }
@@ -334,8 +419,8 @@ impl Div for DimensionedValue {
     fn div(self, rhs: Self) -> Self {
         println!("\n\nDividing {:#?} into {:#?}", self, rhs);
 
-        let lhs_in_base_units = self.value * Value::Float(self.unit.conversion_rate);
-        let rhs_in_base_units = rhs.value * Value::Float(rhs.unit.conversion_rate);
+        let lhs_in_base_units = self.value * Value::Float(self.unit.conversion_rate());
+        let rhs_in_base_units = rhs.value * Value::Float(rhs.unit.conversion_rate());
 
         let value = lhs_in_base_units / rhs_in_base_units;
 
@@ -345,6 +430,8 @@ impl Div for DimensionedValue {
             value: value,
             unit: Unit {
                 dimension: self.unit.dimension / rhs.unit.dimension,
+                exponent: self.unit.exponent,
+                unit: self.unit.unit / rhs.unit.unit,
             },
         }
     }
